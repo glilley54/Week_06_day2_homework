eb5f9c6857c9cc1014393c17b61d8e0f0f815f29
glilley54 <glilley54@gmail.com>
2021-05-04 19:53:47 +0100

    attempted calulating number visitors

--- models/park.js
+++ models/park.js
@@ -13,4 +13,10 @@ Park.prototype.removeDinosaur = function (dino) {
     this.dinoCollection.pop(dino);
 };
 
+Park.prototype.totalVisitorsPerYear = function (dino) {
+
+    dinosaur.guestsAttractedPerDay * 365;
+
+
+
     module.exports = Park;
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -44,15 +44,24 @@ describe('Park', function() {
   });
 
 
+  // xit('should be able to find the dinosaur that attracts the most visitors', function (){
+  //   // park.mostPopularDinosaur(dino)
 
-  it('should be able to find the dinosaur that attracts the most visitors');
+  
 
-  it('should be able to find all dinosaurs of a particular species');
 
-  it('should be able to calculate the total number of visitors per day');
+  // xit('should be able to find all dinosaurs of a particular species');
 
-  it('should be able to calculate the total number of visitors per year');
 
-  it('should be able to calculate total revenue for one year');
+  // xit('should be able to calculate the total number of visitors per day');
 
-});
+  it('should be able to calculate the total number of visitors per year', function () {
+    park.totalvisitorsperyear(dino1);
+    const expected = 7300;
+    assert.strictEqual(park.totalVisitorsPerYear(), expected);
+  });
+
+
+  // xit('should be able to calculate total revenue for one year');
+
+  });
